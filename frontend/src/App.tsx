9a9af3173946e7e9b8628e9cea9f3b0c4af01e1a
AgonHamiti <ah51397@ubt-uni.net>
2024-09-05 22:48:30 +0200

    edit my hotel hotel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,13 @@ import {
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import { useAppContext } from "./contexts/AppContext";
+import AddHotel from "./pages/AddHotel";
 
 
 
 const App = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <Router>
       <Routes>
@@ -46,9 +49,23 @@ const App = () => {
             </Layout>
           }
         />
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+           
+           </>
+          )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
+    
   );
 };
 
